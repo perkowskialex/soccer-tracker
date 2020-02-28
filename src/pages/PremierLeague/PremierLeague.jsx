5270f99eae7c489f4d8653c1798b1bbf10560a11
Alex Perkowski <perkowskialex@gmail.com>
2020-02-28 15:59:21 -0600

    premier league fetch functioning

--- src/pages/PremierLeague/PremierLeague.jsx
+++ src/pages/PremierLeague/PremierLeague.jsx
@@ -13,21 +13,36 @@ export default class PremierLeague extends React.Component {
   componentDidMount() {
     //fetch Premier League
     fetch("https://api-football-v1.p.rapidapi.com/v2/teams/league/2", {
-      method: "GET",
       headers: {
         "x-rapidapi-host": "api-football-v1.p.rapidapi.com",
         "x-rapidapi-key": "ffc04b5815mshc0cd65f93188805p1a1234jsnc8f5c54fcef3"
       }
     })
-      .then(response => {
-        console.log(response);
+      .then(res => {
+        return res.json().then(data => {
+          // gets {api}
+          console.log(data);
+          return data;
+        });
+      })
+      .then(data => {
+        this.setState({ teams: data.api.teams });
+        console.log(this.state.teams);
       })
       .catch(err => {
         console.log(err);
       });
   }
+  
+  const premierLeagueMap = () => {
+
+  }
 
   render() {
-    return <h1>Premier League</h1>;
+    return (
+      <div>
+        <h1>Premier League Teams</h1>
+      </div>
+    );
   }
 }
