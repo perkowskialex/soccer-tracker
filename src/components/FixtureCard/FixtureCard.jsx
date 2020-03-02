4b3405dcab1af00710570fb38657dacdb611b6f7
Alex Perkowski <perkowskialex@gmail.com>
2020-03-02 16:24:08 -0600

    add date and time

--- src/components/FixtureCard/FixtureCard.jsx
+++ src/components/FixtureCard/FixtureCard.jsx
@@ -10,9 +10,12 @@ export const FixtureCard = props => {
         <img src={`${props.f.homeTeam.logo}`} alt="Home Team Logo" />
         <img src={`${props.f.awayTeam.logo}`} alt="Away Team Logo" />
       </div>
-      <h2>
-        {props.f.homeTeam.team_name} v. {props.f.awayTeam.team_name}
-      </h2>
+      <span>
+        <h2>{props.f.homeTeam.team_name}</h2> <p>vs</p>{" "}
+        <h2>{props.f.awayTeam.team_name}</h2>
+      </span>
+      <h4>Event Date and Time: {props.f.event_date}</h4>
+      <h4>Location: {props.f.venue}</h4>
     </div>
   );
 };
