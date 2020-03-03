63d79229fb778ed94ec6d29bdc6aa6b8c69f21ac
Alex Perkowski <perkowskialex@gmail.com>
2020-03-03 13:27:27 -0600

    intro redux

--- src/components/TeamButton/TeamButton.jsx
+++ src/components/TeamButton/TeamButton.jsx
@@ -5,7 +5,7 @@ import "./TeamButton.css";
 
 const TeamButton = props => {
   return (
-    <Link to={`/pl/${props.team.team_id}`}>
+    <Link to={{ pathname: `/pl/${props.team.team_id}` }}>
       <button>View {props.team.name}</button>
     </Link>
   );
