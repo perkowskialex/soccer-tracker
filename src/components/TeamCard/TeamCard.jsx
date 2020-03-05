7f5c442a391a7804d1123071a9732d28b1176bca
Alex Perkowski <perkowskialex@gmail.com>
2020-03-05 13:43:16 -0600

    convert team card to component

--- src/components/TeamCard/TeamCard.jsx
+++ src/components/TeamCard/TeamCard.jsx
@@ -2,29 +2,34 @@ import React from "react";
 import "./TeamCard.css";
 import TeamFixtures from "../TeamFixtures/TeamFixtures";
 
-const TeamCard = props => {
-  console.log("team card props", props);
+class TeamCard extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log("team card props", props);
+  }
   // const teamId = props.team.team_id;
-  return (
-    <div className="team-card-container">
-      {" "}
-      <img
-        className="image"
-        src={`https://media.api-sports.io/teams/${props.team.team_id}.png`}
-        alt="Team Logo"
-      />
-      <h1>{props.team.name}</h1>
-      <p>Est. {props.team.founded}</p>
-      <p>{props.team.venue_name}</p>
-      <p>
-        Location: {props.team.venue_address}, {props.team.venue_city}
-      </p>
-      <div>
-        <h2>{props.team.name}'s upcoming fixtures:</h2>
-        <TeamFixtures team={props.team} />
+  render() {
+    return (
+      <div className="team-card-container">
+        {" "}
+        <img
+          className="image"
+          src={`https://media.api-sports.io/teams/${props.team.team_id}.png`}
+          alt="Team Logo"
+        />
+        <h1>{props.team.name}</h1>
+        <p>Est. {props.team.founded}</p>
+        <p>{props.team.venue_name}</p>
+        <p>
+          Location: {props.team.venue_address}, {props.team.venue_city}
+        </p>
+        <div>
+          <h2>{props.team.name}'s upcoming fixtures:</h2>
+          <TeamFixtures team={props.team} />
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default TeamCard;
