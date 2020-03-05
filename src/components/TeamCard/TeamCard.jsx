e2c422c879553169cb02eb4289e12f5fca3f0a7e
Alex Perkowski <perkowskialex@gmail.com>
2020-03-05 14:28:44 -0600

    fetch coach

--- src/components/TeamCard/TeamCard.jsx
+++ src/components/TeamCard/TeamCard.jsx
@@ -6,7 +6,34 @@ class TeamCard extends React.Component {
   constructor(props) {
     super(props);
     console.log("team card props", props);
+    this.state = {
+      coach: ""
+    };
+    fetch(
+      `https://api-football-v1.p.rapidapi.com/v2/coachs/team/${this.props.team.team_id}`,
+      {
+        method: "GET",
+        headers: {
+          "x-rapidapi-host": "api-football-v1.p.rapidapi.com",
+          "x-rapidapi-key": "8b54882d16msh91800fa15b1c3cep15cb26jsna5a0b982eaa6"
+        }
+      }
+    )
+      .then(res => {
+        return res.json().then(data => {
+          // gets {api}
+          return data;
+        });
+      })
+      .then(data => {
+        this.setState({ coach: data.api.coachs[0] });
+        console.log(this.state.coach);
+      })
+      .catch(err => {
+        console.log(err);
+      });
   }
+
   // const teamId = props.team.team_id;
   render() {
     return (
@@ -14,18 +41,19 @@ class TeamCard extends React.Component {
         {" "}
         <img
           className="image"
-          src={`https://media.api-sports.io/teams/${props.team.team_id}.png`}
+          src={`https://media.api-sports.io/teams/${this.props.team.team_id}.png`}
           alt="Team Logo"
         />
-        <h1>{props.team.name}</h1>
-        <p>Est. {props.team.founded}</p>
-        <p>{props.team.venue_name}</p>
+        <h1>{this.props.team.name}</h1>
+        <p>Est. {this.props.team.founded}</p>
+        <p>{this.props.team.venue_name}</p>
         <p>
-          Location: {props.team.venue_address}, {props.team.venue_city}
+          Location: {this.props.team.venue_address},{" "}
+          {this.props.team.venue_city}
         </p>
         <div>
-          <h2>{props.team.name}'s upcoming fixtures:</h2>
-          <TeamFixtures team={props.team} />
+          <h2>{this.props.team.name}'s upcoming fixtures:</h2>
+          <TeamFixtures team={this.props.team} />
         </div>
       </div>
     );
