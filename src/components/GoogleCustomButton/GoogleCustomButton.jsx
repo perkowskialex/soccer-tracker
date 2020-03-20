8e0c82a5df98a4cac8c67f2674897857c673717c
Alex Perkowski <perkowskialex@gmail.com>
2020-03-20 16:28:32 -0500

    set up google custom button

---
+++ src/components/GoogleCustomButton/GoogleCustomButton.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./GoogleCustomButton.css";
+import { googleSignIn, googleSignOut } from "../../firebase/auth";
+
+export const GoogleCustomButton = () => {
+  return <div>Google Button</div>;
+};
